e4dbf801382561126971584c78ca84835b0887c1
Janice <janice.vilela@gmail.com>
2025-02-19 11:04:30 +0100

    fixing Github Actions to Deply

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from 'node-polyfill-webpack-plugin';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/randomizer/",
-})
+  plugins: [react(), NodeGlobalsPolyfillPlugin({})],
+  base: '/randomizer/',
+});
