e199145043bd4b8070ee4962d81aceade6ed4d81
Janice <janice.vilela@gmail.com>
2025-02-19 12:36:16 +0100

    fixing Github Actions to Deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
 
 export default defineConfig({
-  plugins: [react(), NodePolyfillPlugin()],
+  plugins: [react(), new NodePolyfillPlugin()],
   base: '/randomizer/',
 });
